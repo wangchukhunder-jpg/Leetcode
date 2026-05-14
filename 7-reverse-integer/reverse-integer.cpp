e5c592cbfab51b853871539bfb7caf99873bc185
wangchukhunder-jpg <wangchukhunder@gmail.com>
2026-05-14 18:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.47%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0;
+        while(x!=0){
+            int ld=x%10;
+            rev=rev*10+ld;
+            x=x/10;
+        }
+        if(rev>INT_MAX||rev<INT_MIN)
+        return 0;
+        return rev;    
+        
+    }
+};
