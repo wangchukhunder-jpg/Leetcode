3cf86043486b3c7019512648000af750e3461f55
wangchukhunder-jpg <wangchukhunder@gmail.com>
2026-05-26 11:41:25 +0530

    Time: 3 ms (19.07%) | Memory: 50.3 MB (32.30%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0;int maxi;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+           if(nums[i]==1){
+            cnt++;
+            maxi=max(maxi,cnt);
+           }else{
+            cnt=0;
+           }
+        }return maxi;
+    }
+};
