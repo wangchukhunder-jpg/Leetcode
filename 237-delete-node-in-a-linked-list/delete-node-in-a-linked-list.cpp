ca4d40aad39728845074f92a2531ff1c596e7bbc
wangchukhunder-jpg <wangchukhunder@gmail.com>
2026-07-05 21:49:06 +0530

    Time: 10 ms (36.86%) | Memory: 12.4 MB (56.32%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -0,0 +1,18 @@
+
+ 
+ /** struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        ListNode*temp=node->next;
+        node->next=node->next->next;
+        delete temp;
+    }
+};
